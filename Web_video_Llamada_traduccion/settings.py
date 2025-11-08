d1621dde1fcdfd76630935d2e574f5bbd8b04001
Luis <lpoloniol@unemi.edu.ec>
2025-11-07 22:43:51 -0500

    Ajustes para que ahora si funcione x2

--- Web_video_Llamada_traduccion/settings.py
+++ Web_video_Llamada_traduccion/settings.py
@@ -66,11 +66,8 @@ INSTALLED_APPS = [
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],  # Redis corre en localhost
-        },
-    },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
 
 MIDDLEWARE = [
